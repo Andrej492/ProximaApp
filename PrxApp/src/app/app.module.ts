b4f2f8313f3c32d7485702884f5defcf1600b820
Andrej492 <andrejmatovic1@gmail.com>
2022-03-04 11:53:41 +0100

    added translate languages, settings, still setting configuration for translate module

--- PrxApp/src/app/app.module.ts
+++ PrxApp/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { TranslateModule } from '@ngx-translate/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,9 @@ import { UserDetailComponent } from './home/user-detail/user-detail.component';
 import { UserListComponent } from './home/user-list/user-list.component';
 import { UserItemComponent } from './home/user-list/user-item/user-item.component';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -31,7 +35,13 @@ import { UserItemComponent } from './home/user-list/user-item/user-item.componen
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    TranslateModule.forRoot()
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
